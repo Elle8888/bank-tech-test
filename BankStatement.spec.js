babb6f37c3fac3f53cd6b960939b27f67f9a9e00
Elle <lou.d.b.rigodon@gmail.com>
2023-01-23 20:35:42 +0000

    add more test

--- BankStatement.spec.js
+++ BankStatement.spec.js
@@ -7,17 +7,53 @@ describe("BankStatement functions", () => {
   transactions = [];
 });
 
-  it('printBankStatement should print all transactions', () => {
+  it('printBankStatement should print credit transaction with updated balance', () => {
     const transaction1 = {date: new Date(), credit:500, debit: 0};
     transactions.push(transaction1);
     balance += 500;
-
     console.log = jest.fn();
-
     printBankStatements();
-
     const statement = `${transaction1.date} || ${transaction1.credit} || ${transaction1.debit} || ${balance}`;
-    // expect(printBankStatements).toEqual(statement);
+    expect(console.log).toHaveBeenCalledWith(statement)
+  })
+
+  it('printBankStatement should print debit transaction with updated balance', () => {
+    const transaction1 = {date: new Date(), credit: 0, debit: 500};
+    transactions.push(transaction1);
+    balance -= 500;
+    console.log = jest.fn();
+    printBankStatements();
+    const statement = `${transaction1.date} || ${transaction1.credit} || ${transaction1.debit} || ${balance}`;
+    expect(console.log).toHaveBeenCalledWith(statement)
+  })
+
+  it('printBankStatement should updated balance for multiple transactions', () => {
+    const transaction1 = {date: new Date(), credit: 1000, debit: 0};
+    transactions.push(transaction1);
+    balance += 500;
+    const transaction2 = {date: new Date(), credit: 0, debit: 50};
+    transactions.push(transaction2);
+    balance -= 50;
+    let outputData = "Date || Credit || Debit || Balance";
+    transactions.forEach((transaction) => {
+      let currentBalance = balance;
+      currentBalance = currentBalance - transaction.debit + transaction.credit;
+      outputData += `${transaction.date} || ${transaction.credit} || ${transaction.debit} || ${currentBalance}\n`;
+    });
+    console.log = jest.fn((data) => {
+      outputData += data;
+    });
+    printBankStatements();
+    expect(outputData).toEqual(outputData);
+  });
+
+  it('printBankStatement should print empty statement for zero transactions', () => {
+    const transaction1 = {date: new Date(), credit: 0, debit: 0};
+    transactions.push(transaction1);
+    balance += 0;
+    console.log = jest.fn();
+    printBankStatements();
+    const statement = `${transaction1.date} || ${transaction1.credit} || ${transaction1.debit} || ${balance}`;
     expect(console.log).toHaveBeenCalledWith(statement)
   })
 });
