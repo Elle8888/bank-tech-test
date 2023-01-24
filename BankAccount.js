7a89233bfb8c5505a86f6c6bc282daffc5696bef
Elle <lou.d.b.rigodon@gmail.com>
2023-01-24 11:42:51 +0000

    Fix current balance error

--- BankAccount.js
+++ BankAccount.js
@@ -8,11 +8,11 @@ class BankAccount {
     if (this.transactions.length === 0) {
       return console.log("No transactions to display");
     }
-    let currentBalance = balance;
+    let currentBalance = 0;
     this.transactions.forEach((transaction) => {
       let credit = transaction.credit || 0;
       let debit = transaction.debit || 0;
-      currentBalance = currentBalance - debit + credit;
+      currentBalance = currentBalance + credit - debit;
       return console.log(`${transaction.date} || ${credit} || ${debit} || ${currentBalance}`);
       });
   }
