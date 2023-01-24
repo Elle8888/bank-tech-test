fb91347e97ef2619309e2e0aa06189efda398f76
Elle <lou.d.b.rigodon@gmail.com>
2023-01-24 18:08:44 +0000

    Move format date to a separate function

--- BankAccount.js
+++ BankAccount.js
@@ -18,6 +18,10 @@ class BankAccount {
       this.transactions.unshift({date: new Date(), credit: null, debit: amount, asOfBalance: this.balance});
   }
 
+  formatDate(date) {
+    return date.toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "numeric"});
+  }
+
   printBankStatements() {
     if (this.transactions.length === 0) {
       return console.log("No transactions to display");
@@ -27,7 +31,7 @@ class BankAccount {
       let credit = transaction.credit || 0;
       let debit = transaction.debit || 0;
       let currentBalance = transaction.asOfBalance || 0;
-      let formattedDate = transaction.date.toLocaleDateString("en-GB", { day: "2-digit", month: "2-digit", year: "numeric"});
+      let formattedDate = this.formatDate(transaction.date);
       return console.log(`${formattedDate} || ${credit} || ${debit} || ${currentBalance}`);
       });
   }
